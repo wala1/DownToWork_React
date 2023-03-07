e9e03e553f39fd84f10220c066e4b76b8f08eff2
wala1 <wala.alimi@esprit.tn>
2023-03-07 20:41:53 +0100

    integrate template 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,20 @@
-import logo from '../assets/images/logo.svg';
+import { Fragment } from 'react';
 import '../style/App.css';
+import Home from './home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="body_wrap">
+        <div className="page_wrap">
+          <Home />
+        </div>
+      </div>
+
+    </Fragment>
+
+
+
   );
 }
 
