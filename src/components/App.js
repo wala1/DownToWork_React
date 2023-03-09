b2fbd674efb8b3c80a09e806c5934654e1f719f2
wala1 <wala.alimi@esprit.tn>
2023-03-09 13:22:25 +0100

    update templae

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import { Fragment } from 'react';
 import '../style/App.css';
 import Home from './home';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <Fragment>
       <div className="body_wrap">
         <div className="page_wrap">
-          <Home />
+          {/* <Home /> */}
+          <NavBar/>
         </div>
       </div>
 
