fdb2ca8ddd5ea5579eb678fa9772a3631d71cc09
Taher <taher.snoussi@esprit.tn>
2023-03-11 14:28:19 +0100

    auth fix

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -37,7 +37,7 @@ const handleLogout =async () => {
 										<span className="user_avatar">
 											<img alt="" src="http://1.gravatar.com/avatar/45e4d63993e55fa97a27d49164bce80f?s=16&#038;d=mm&#038;r=g" srcSet="http://1.gravatar.com/avatar/45e4d63993e55fa97a27d49164bce80f?s=32&amp;d=mm&amp;r=g 2x" className="avatar avatar-16 photo" height="16" width="16" />
 										</span>
-										<span className="user_name">{user.name}</span></a>
+										<span className="user_name">{user?.name || "taher"}</span></a>
                                     <ul>
                                         <li><a href="#" className="icon icon-doc-inv">New post</a></li>
                                         <li><a href="#" className="icon icon-cog-1">Settings</a></li>
