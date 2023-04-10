cbcb2c270b85b1ebc4ea5c99bf287fee2ce43499
Taher <taher.snoussi@esprit.tn>
2023-04-10 04:03:54 +0200

    chart

--- src/components/BusinessDashboard/SalesDashboard.jsx
+++ src/components/BusinessDashboard/SalesDashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SalesTable from './SalesTable.jsx'
 import IncomeItem from './IncomItem.jsx'
 import TransactionsItem from './Transactions.jsx'
+import Chart from './Chart.jsx'
 function SalesDashboard() {
   return (
     <div style={{display:"flex" , flexDirection:"column",paddingTop:90}}>
@@ -9,6 +10,8 @@ function SalesDashboard() {
       <div><IncomeItem/></div>
       {/* <div><TransactionsItem/></div> */}
       </div>
+      <div style={{marginTop:30}}><Chart/></div>
+
        
         <div><SalesTable/></div>
     </div>
