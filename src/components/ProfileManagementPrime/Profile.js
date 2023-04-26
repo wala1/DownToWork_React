63cee62be9a77a6a1b32c5e1194b3cdc48e36ef7
Ons <ons.diweni@esprit.tn>
2023-04-26 21:46:28 +0100

    Updating user ui

--- src/components/ProfileManagementPrime/Profile.js
+++ src/components/ProfileManagementPrime/Profile.js
@@ -7,19 +7,26 @@ import AddPost from './AddPost';
 import MyPosts from './MyPosts';
 import Side from './Side';
 
+
 const Team = () => {
 
-  const userString = localStorage.getItem ('user');
-  const user = JSON.parse (userString);
-  const img =
+const userString = localStorage.getItem ('user');
+const user = JSON.parse (userString);
+const img =
     'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEzNzYzNn0?utm_source=dictionnaire&utm_medium=referral';
 
-  const [post, setPost] = useState("");
-  const [posts, setPosts] = useState([]);
-  const urlGetPosts = 'http://localhost:3001/post/getAllByUser/';
-  const urlAddPost = 'http://127.0.0.1:3001/post/add';
+const token = localStorage.getItem("token");
+const config = {
+headers: { Authorization: `Bearer ${token}` }
+};   
 
-  useEffect(() => {
+const [post, setPost] = useState("");
+const [posts, setPosts] = useState([]);
+const urlGetPosts = 'http://localhost:3001/post/getAllByUser/';
+const urlAddPost = 'http://127.0.0.1:3001/post/add';
+const urlDeletePost = 'http://127.0.0.1:3001/post/delete/';
+
+useEffect(() => {
         // appel API pour récupérer les publications existantes
         axios.get(urlGetPosts + user._id).then((response) => {
           setPosts(response.data);
@@ -28,18 +35,18 @@ const Team = () => {
       }, []);
 
 
-  function deleteElement (index) {
-    const updatedElements = [...posts];
-    updatedElements.splice (index, 1);
-    setPosts (updatedElements);
-  }
+function deleteElement (id) {
+        axios.delete(urlDeletePost + id, config).then(() => {
+          console.log('post deleted')
+          const updatedPosts = posts.filter(post => post._id !== id);
+          setPosts(updatedPosts);
+        }).catch(error => {
+          console.log(error);
+        });
+      }
 
-  const addItem = (newPost) => {   
+const addItem = (newPost) => {   
 /*  const newPost = {text : post, name :user.name , user : user._id} */
- const token = localStorage.getItem("token");
- const config = {
-  headers: { Authorization: `Bearer ${token}` }
- };
     // appel API pour publier la publication
   axios.post(urlAddPost, newPost , config ).then((response) => {
       // ajouter le nouveau message à la liste des messages
